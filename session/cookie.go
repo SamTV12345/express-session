ba10bb7673120356d4a89fcdc9a1a6cd852910aa
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2025-05-01 09:27:54 +0200

    Continued with cookie

--- session/cookie.go
+++ session/cookie.go
@@ -1,13 +1,23 @@
 package session
 
 import (
-	cookie "github.com/SamTV12345/cookie"
+	"github.com/SamTV12345/cookie"
 	"time"
 )
 
+type CookieParam struct {
+	Path           *string
+	HttpOnly       *bool
+	OriginalMaxAge *int64
+	Domain         *string
+	SameSite       *string
+	expires        *time.Time
+	originalMaxAge *int64
+	Secure         *bool
+}
+
 type Cookie struct {
 	Path           string
-	MaxAge         *int64
 	HttpOnly       bool
 	OriginalMaxAge *int64
 	Domain         string
@@ -20,12 +30,31 @@ type Cookie struct {
 func NewCookie(options interface{}) Cookie {
 	var sessCookie = Cookie{
 		Path:     "/",
-		MaxAge:   nil,
 		HttpOnly: true,
 	}
+	sessCookie.SetMaxAge(nil)
 
-	if options != nil {
-		// todo
+	if options != nil && options.(*CookieParam) != nil {
+		var optionsCookie = options.(*CookieParam)
+		if optionsCookie.Path != nil {
+			sessCookie.Path = *optionsCookie.Path
+		}
+		if optionsCookie.HttpOnly != nil {
+			sessCookie.HttpOnly = *optionsCookie.HttpOnly
+		}
+		if optionsCookie.expires != nil {
+			sessCookie.expires = optionsCookie.expires
+		}
+
+		if optionsCookie.Domain != nil {
+			sessCookie.Domain = *optionsCookie.Domain
+		}
+		if optionsCookie.SameSite != nil {
+			sessCookie.SameSite = *optionsCookie.SameSite
+		}
+		if optionsCookie.Secure != nil {
+			sessCookie.Secure = *optionsCookie.Secure
+		}
 	}
 
 	if sessCookie.OriginalMaxAge == nil {
@@ -56,6 +85,10 @@ func (c *Cookie) GetMaxAge() *int64 {
 }
 
 func (c *Cookie) SetMaxAge(ms *int64) {
+	if ms == nil {
+		c.expires = nil
+		return
+	}
 	var newExpires = time.UnixMilli(time.Now().UnixMilli() + *ms)
 	c.expires = &newExpires
 }
--- session/cookie_test.go
+++ session/cookie_test.go
@@ -1,6 +1,9 @@
 package session
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestNewCookie(t *testing.T) {
 	var cookie = NewCookie(nil)
@@ -26,6 +29,49 @@ func TestShouldDefaultPathToSlash(t *testing.T) {
 func TestShouldDefaultMaxAgeToNull(t *testing.T) {
 	var cookie = NewCookie(nil)
 	if cookie.GetMaxAge() != nil {
-		t.Error("Expected nil but got", cookie.MaxAge)
+		t.Error("Expected nil but got", cookie.GetMaxAge())
+	}
+}
+
+func TestShouldCreateCookieWithOptions(t *testing.T) {
+	var result = NewCookie(&CookieParam{})
+	if result.GetMaxAge() != nil {
+		t.Error("Expected nil but got", result.GetMaxAge())
+	}
+}
+
+func TestShouldSetExpires(t *testing.T) {
+	var expires = time.UnixMilli(time.Now().UnixMilli() + 60000)
+	var cookie = NewCookie(&CookieParam{
+		expires: &expires,
+	})
+	if cookie.expires.UnixMilli() != expires.UnixMilli() {
+		t.Error("Expected", expires, "but got", cookie.expires)
+	}
+}
+
+func TestShouldSetMaxAge(t *testing.T) {
+	expires := time.UnixMilli(time.Now().UnixMilli() + 60000)
+	cookie := NewCookie(&CookieParam{
+		expires: &expires,
+	})
+
+	if !(expires.UnixMilli()-time.Now().UnixMilli()-1000 <= *cookie.GetMaxAge()) {
+		t.Error("Expected", expires, "but got", cookie.expires)
+	}
+
+	if !(expires.UnixMilli()-time.Now().UnixMilli()+1000 <= *cookie.GetMaxAge()) {
+		t.Error("Expected", expires, "but got", cookie.expires)
+	}
+}
+
+func TestShouldSetHttOnly(t *testing.T) {
+	var httpOnly = false
+	var cookie = NewCookie(&CookieParam{
+		HttpOnly: &httpOnly,
+	})
+
+	if cookie.HttpOnly != false {
+		t.Error("Expected false but got", cookie.HttpOnly)
 	}
 }
